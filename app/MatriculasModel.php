ba948c8f6d5db5e0a2e591898e5313eaecdd1e83
sanmen1593 <contacto@santiagomendoza.org>
2016-06-02 16:11:44 -0500

    Agregar funcion enrolledAs

--- app/MatriculasModel.php
+++ app/MatriculasModel.php
@@ -1,4 +1,6 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -7,16 +9,25 @@ class MatriculasModel extends Model {
     protected $table = 'MATRICULA_EXTERNA_SAVIO';
     protected $connection = 'sirius_bd';
 
-    public function course(){
+    public static function enrolledAs($NRC, $username) {
+        $matricula = MatriculasModel::where("IDNUMBER", "=", $NRC)->where('USERNAME', '=', $username)->first();
+        if ($matricula) {
+            return $matricula->ROLE;
+        } else {
+            return null;
+        }
+    }
+
+    public function course() {
         return $this->belongsTo('App\CoursesModel', 'IDNUMBER', 'NRC_PERIODO_KEY');
     }
 
-    public function student(){
-        return  $this->belongsTo('App\StudentsModel', 'USERNAME', 'ID');
+    public function student() {
+        return $this->belongsTo('App\StudentsModel', 'USERNAME', 'ID');
     }
 
-    public function teacher(){
-        return  $this->belongsTo('App\TeachersModel', 'USERNAME', 'ID');
+    public function teacher() {
+        return $this->belongsTo('App\TeachersModel', 'USERNAME', 'ID');
     }
 
 }
